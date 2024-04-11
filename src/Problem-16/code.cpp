3b13836caf7edfb701ce140e02e6e553c1280e48
Aditya Chatterjee <AdiChat@users.noreply.github.com>
2024-04-11 23:48:51 +0530

    Code - P16

---
+++ src/Problem-16/code.cpp
@@ -0,0 +1,66 @@
+/* **************************************************************************
+ * Copyright 2024 The OpenGenus.org Authors. All Rights Reserved.
+ *
+ * Code for the book "DAILY 43: Algorithmic Problems for Coding Interviews: Easy level, C++ edition"
+ *
+ * Licensed under the GNU General Public License, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For details about the book, please visit: https://www.amazon.com/dp/B0CZJNBLQS
+ * *************************************************************************/
+
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode* reverseList(ListNode* head) {
+    ListNode *nextNode, *prevNode = NULL;
+    while (head) {
+        nextNode = head->next;
+        head->next = prevNode;
+        prevNode = head;
+        head = nextNode;
+    }
+    // New head of the reversed list
+    return prevNode;
+}
+
+void printList(ListNode* head) {
+    ListNode* current = head;
+    while (current) {
+        std::cout << current->val << " ";
+        current = current->next;
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    std::cout << "Original list: ";
+    printList(head);
+
+    ListNode* reversedHead = reverseList(head);
+
+    std::cout << "Reversed list: ";
+    printList(reversedHead);
+
+    return 0;
+}
