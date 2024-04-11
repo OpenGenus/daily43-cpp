1fb66e5287104790039b802dbde25380971d6cce
Aditya Chatterjee <AdiChat@users.noreply.github.com>
2024-04-12 00:00:20 +0530

    Code - P21

---
+++ src/Problem-21/code.cpp
@@ -0,0 +1,61 @@
+/* **************************************************************************
+ * Copyright 2024 The OpenGenus.org Authors. All Rights Reserved.
+ *
+ * Code for the book "DAILY 43: Algorithmic Problems for Coding Interviews: Easy level, C++ edition"
+ *
+ * Licensed under the GNU General Public License, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For details about the book, please visit: https://www.amazon.com/dp/B0CZJNBLQS
+ * *************************************************************************/
+
+#include <iostream>
+
+// Definition for a singly-linked list node
+class ListNode {
+public:
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
+
+int main() {
+    // Create a sample linked list
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    // Create an instance of the Solution class
+    Solution sol;
+
+    // Find the middle node of the linked list
+    ListNode* middle = sol.middleNode(head);
+
+    // Print the value of the middle node
+    std::cout << "Middle node value: " << middle->val << std::endl;
+
+    return 0;
+}
