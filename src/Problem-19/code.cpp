5ace5cd1df5bf156a3716db082ff00c20088b587
Aditya Chatterjee <AdiChat@users.noreply.github.com>
2024-04-11 23:55:52 +0530

    Code - P19

---
+++ src/Problem-19/code.cpp
@@ -0,0 +1,53 @@
+/* **************************************************************************
+ * Copyright 2024 The OpenGenus.org Authors. All Rights Reserved.
+ *
+ * Code for the book "DAILY 43: Algorithmic Problems for Coding Interviews: Easy level, C++ edition"
+ *
+ * Licensed under the GNU General Public License, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For details about the book, please visit: https://www.amazon.com/dp/B0CZJNBLQS
+ * *************************************************************************/
+
+#include <iostream>
+#include <algorithm>
+
+// Definition for a binary tree node
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+int maxDepth(TreeNode* root) {
+    int maxLeft = 0, maxRight = 0;
+    if (!root)
+        return 0;
+    maxLeft = maxDepth(root->left);
+    maxRight = maxDepth(root->right);
+    return std::max(maxLeft, maxRight) + 1;
+}
+
+int main() {
+    // Create a sample binary tree
+    TreeNode* root = new TreeNode(3);
+    root->left = new TreeNode(9);
+    root->right = new TreeNode(20);
+    root->right->left = new TreeNode(15);
+    root->right->right = new TreeNode(7);
+
+    // Calculate and print the maximum depth of the binary tree
+    std::cout << "Maximum depth of the binary tree is: " << maxDepth(root) << std::endl;
+
+    return 0;
+}
