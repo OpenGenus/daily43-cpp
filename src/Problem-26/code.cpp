9712d7bdc3c05ee8c3bd8f21e263b602cd44a3b1
Aditya Chatterjee <AdiChat@users.noreply.github.com>
2024-07-01 02:07:30 +0530

    Code + output - P26

---
+++ src/Problem-26/code.cpp
@@ -0,0 +1,77 @@
+/* **************************************************************************
+ * Copyright 2024 The OpenGenus.org Authors. All Rights Reserved.
+ *
+ * Code for the book "DAILY 43: Algorithmic Problems for Coding Interviews: Easy level, C++ edition"
+ *
+ * Licensed under the GNU General Public License, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For details about the book, please visit: https://www.amazon.com/dp/B0CZJNBLQS
+ * *************************************************************************/
+
+#include <iostream>
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool isSameBinaryTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr) {
+            return true;
+        }
+        if (p == nullptr || q == nullptr) {
+            return false;
+        }
+        if (p->val == q->val) {
+            return isSameBinaryTree(p->left, q->left) && 
+                   isSameBinaryTree(p->right, q->right);
+        }
+        return false;
+    }
+};
+
+int main() {
+    // Example usage
+    Solution sol;
+
+    TreeNode* tree1 = new TreeNode(1);
+    tree1->left = new TreeNode(2);
+    tree1->right = new TreeNode(3);
+
+    TreeNode* tree2 = new TreeNode(1);
+    tree2->left = new TreeNode(2);
+    tree2->right = new TreeNode(3);
+
+    if (sol.isSameBinaryTree(tree1, tree2)) {
+        std::cout << "The trees are the same." << std::endl;
+    } else {
+        std::cout << "The trees are not the same." << std::endl;
+    }
+
+    // Clean up
+    delete tree1->left;
+    delete tree1->right;
+    delete tree1;
+    delete tree2->left;
+    delete tree2->right;
+    delete tree2;
+
+    return 0;
+}
